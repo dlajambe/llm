15d39445d13d63454d9952b6956471727ca4954a
David LaJambe <dlajambe@gmail.com>
2023-09-26 21:29:57 -0400

    Add hyperparameters

--- src/llm.py
+++ src/llm.py
@@ -1,4 +1,12 @@
 import torch
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Device: {}'.format(device))
+
+# Step 0 - Hyperparameters
+block_size = 8
+batch_size = 32
+print('Block size: {}'.format(block_size))
+print('Batch size: {}'.format(batch_size))
 
 # Step 1 - Import the data
 with open('data/wizard_of_oz.txt', 'r', encoding='utf-8') as f:
@@ -21,7 +29,8 @@ class Encoder():
     
 encoder = Encoder(chars)
 data = encoder.encode(text)
-print(data[:50])
+print('Unencoded data sample: {}'.format(text[:10]))
+print('Encoded data sample: {}'.format(data[:10]))
 
 # Step 3 - Split data into training and testing partitions
 # training: Used to fit the model
@@ -32,4 +41,7 @@ n_train = int(len(data) * train_frac)
 train = [i < n_train for i in range(len(data))]
 test = [i >= n_train for i in range(len(data))]
 
-# Step 4 - Implement the bigram model
+# Step 4 - Use a sliding window approach to model 
+
+
+# Step 5 - Train the model
