Fix error in Head weight normalization

src/modules/data_helpers.py
@@ -22,9 +22,32 @@ class NGramDataSet(Dataset):
         return self.X[idx, :], self.y[idx]
     
 def get_batch(
-        data: torch.long, 
+        data: torch.Tensor, 
         batch_size: int, 
         block_size: int) -> torch.long:
+    """Retrieves a single batch of x and y data from the provided 
+    sequence data.
+
+    The starting points of the sequences comprising the batch are drawn
+    randomly from a uniform distribution.
+
+    Parameters
+    ----------
+    data : Tensor
+        A 1D tensor containing the sequence data from the batch is to be
+        created.
+
+    batch_size : int
+        The number of samples (rows) to be included in the batch.
+    
+    block_size : int
+        The sequence length of each sample in the batch.
+    Returns
+    -------
+    tuple[Tensor, Tensor]
+        The x and y tensors of size (batch_size, block_size) comprising
+        the batch of data.
+    """
     idx = torch.randint(
         low=0, 
         high=len(data) - 1 - block_size, 

src/modules/llm.py
@@ -136,7 +136,7 @@ class Head(nn.Module):
         # Normalizing the weights prevents values from ballooning as the
         # head size increases, which would cause the probabilities to
         # "sharpen" during the softmax step
-        weights = weights * (1 / k.shape[-1] ** (-0.5))
+        weights = weights * (1 / (k.shape[-1] ** (0.5)))
 
         # Ensures that information from the future can not communicate
         # to the past within each individual block of data
@@ -310,6 +310,14 @@ class TransBlock(nn.Module):
         x = x + self.ff(self.ln2(x))
         return x
 
+def inititialize_params(module):
+    if isinstance(module, nn.Linear):
+        torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        if module.bias is not None:
+            torch.nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.Embedding):
+        torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
 class LLM(nn.Module):
     def __init__(self, 
                  block_size: int, 
@@ -335,6 +343,7 @@ class LLM(nn.Module):
         self.output = nn.Linear(head_size * n_heads, vocab_size)
         self.register_buffer('positions', torch.arange(block_size))
         self.block_size = block_size
+        self.apply(inititialize_params)
 
     def forward(self, x: torch.Tensor, y: torch.Tensor=None) -> torch.Tensor:
         if type(x) != torch.Tensor:

src/train_llm.py
@@ -21,11 +21,11 @@ n_trans_blocks = 6
 
 batch_size = 64
 lr = 3e-4
-max_training_iters = 5000
+max_training_iters = 4000
 seed = 1337
 eval_interval = 500
 eval_batches = 200
-dropout_frac = 0.2
+dropout_frac = 0.3
 
 torch.manual_seed(seed)
 